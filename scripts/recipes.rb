52a010688b5d510f3bfb87d7e3896d3fe6c45f41
Orleans <28973224+OrleansCF@users.noreply.github.com>
2017-07-23 22:16:58 -0700

    Scripts: Add recipes.rb
    Data (crafting): Add number_created attribute. Pull in updates from spreadsheet, manually fix a bunch of extraneous spaces in the spreadsheet and update the format to the one we use again. Next time probably easier just to copy the changes manually by looking at the spreadsheet changelog.

--- scripts/recipes.rb
+++ scripts/recipes.rb
@@ -1,9 +1,9 @@
 require 'roo'
 require 'json'
 
-categories = ["Blacksmith Weapon", "Blacksmith Armor", "Fuel", "Basic Harvesting Tool", "Basic Weapon", "Survivalist", "Leatherworking Armor", "Leatherworking Component", "Woodworking Weapon", "Woodworking Armor", "Component", "Siege Warfare", "Geomancy", "Geomancy Architecutre", "Stonemasonry Component", "Harvesting Tool", "Runemaking Component", "Discipline", "Vessel", "Body Parts", "Accessory", "Jewelcrafting component", "Alchemy Component", "Alchemy Potions", "Meat"]
+c = Roo::Spreadsheet.open('../spreadsheets/crafting.xlsx')
 
-c = Roo::Spreadsheet.open('./crafting.xlsx')
+categories = ["Blacksmith Weapon", "Blacksmith Armor", "Fuel", "Basic Harvesting Tool", "Basic Weapon", "Survivalist", "Leatherworking Armor", "Leatherworking Component", "Woodworking Weapon", "Woodworking Armor", "Component", "Siege Warfare", "Geomancy", "Geomancy Architecture", "Stonemasonry Component", "Harvesting Tool", "Runemaking Component", "Discipline", "Vessel", "Body Parts", "Accessory", "Jewelcrafting component", "Alchemy Component", "Alchemy Potions", "Meat"]
 
 @blob = Hash.new
 
@@ -34,6 +34,7 @@ def get_float(resource)
 end
 
 def get_sub_attribute(resource)
+  # learn to regexp please
   subattribute = resource.partition(/\d/).first.partition(/\[/).last.partition(/\]/).first.chomp.strip
   if subattribute == ""
     nil
@@ -44,92 +45,142 @@ end
 
 def get_name(resource)
   if resource.include?("\n")
-    resource.partition("\n").first
+    resource.partition("\n").first.strip
   else
     resource
   end
 end
 
+def clean(str)
+  str.gsub(/[():]/, "").gsub(/[^0-9A-Z]/i, '_').downcase
+end
 
 c.sheets.each_with_index do |t,i| 
   type = String.new
-  wild_card = false
   empty_row_count = 0
+  parent_type = nil
   parent_recipe = nil
   abort_write = false
+
   (c.sheet(i).first_row .. c.sheet(i).last_row).each do |r| 
+    wild_card = false
     row = c.sheet(i).row(r)
     
+    # set type if a header exists
     if row[0] && categories.include?(row[0])
       type = row[0]
-    elsif r > 70
-      type = "subrecipe"
     end
 
-
-
     if row[0] && row[5] && !row[0].include?("Name") && (row[5] || row[6] || row[7] || row[8] || row[9] || row[10] || row[11] || row[12] || row[13] || row[14])
-      if !@blob[t]
-	@blob[t] = Hash.new
-      end
-      if !@blob[t][type]
-	@blob[t][type] = Hash.new
-      end
-      new_name = String
-      new_name = get_name(row[0])
-      if r > 70
-        attribute = get_sub_attribute(row[0])
-	attribute_impact = get_float(row[0])
-	sub_recipe = true
-      end
 
-      @blob[t].each do |type_name,type_full|
-	type_full.each do |recipe_name,recipe_full|
-	  if recipe_name == new_name
-	    parent_recipe = new_name
+      new_name = get_name(row[0])
+
+      if r < 70
+        if !@blob[t]
+	  @blob[t] = Hash.new
+	end
+	if !@blob[t][type]
+	  @blob[t][type] = Hash.new
+	end
+
+       (0..12).each do |i|
+	  if row[i] && row[i] == "[Wild Card]"
+	    wild_card = true
 	  end
 	end
-      end
 
-      (0..12).each do |i|
-        if row[i] && row[i] == "[Wild Card]"
-          wild_card = true
+	if !abort_write
+	  @blob[t][type].merge!(
+		new_name => {
+		  "name" => new_name,
+		  "profession" => t,
+		  "category" => type,
+		  "success_chance" => row[2],
+		  "difficulty" => row[4],
+                  "number_created" => row[1].to_i
+		})
+	  @blob[t][type][new_name]["components"] = Array.new
+	  (5..11).each do |res_col|
+	    if row[res_col]
+	      @blob[t][type][new_name]["components"] << { name: strip_counts(row[res_col]), amount: get_counts(row[res_col]), required: true} 
+	    end
+	  end
+	  (12..16).each do |res_col|
+	    if row[res_col]
+	      @blob[t][type][new_name]["components"] << { name: strip_counts(row[res_col]), amount: get_counts(row[res_col]), required: false} 
+	    end
+	  end
         end
-      end
+      else
 
-      if !abort_write
-	@blob[t][type].merge!(
-	      new_name => {
-		"Chance" => row[2],
-		"Difficulty" => row[4],
-		"Profession" => t,
-		"Category" => type,
-		"subRecipe" => sub_recipe || nil,
-		"subAttribute" => attribute || nil,
-		"subImpact" => attribute_impact || nil,
-		"parentRecipe" => parent_recipe, 
-		"wildCard" => wild_card || nil
-	      })
-	@blob[t][type][new_name]["Resources"] = Array.new
-	@blob[t][type][new_name]["optionalResources"] = Array.new
-	(5..11).each do |res_col|
+       if !@blob[t]
+          @blob[t] = Hash.new
+        end
+        if !@blob[t][type]
+          @blob[t][type] = Hash.new
+        end
+
+	## set subrecipe attributes
+	if r > 70
+	  attribute = get_sub_attribute(row[0])
+	  attribute_impact = get_float(row[0])
+	  sub_recipe = true
+	end
+
+	## set parent recipe if it exists
+	@blob[t].each do |type_name,type_full|
+	  type_full.each do |recipe_name,recipe_full|
+	    if recipe_name == new_name
+              parent_type = type_name
+	      parent_recipe = new_name
+	    end
+	  end
+	end
+	components = Array.new
+        (5..11).each do |res_col|
 	  if row[res_col]
-	    @blob[t][type][new_name]["Resources"] << { name: strip_counts(row[res_col]), count: get_counts(row[res_col])} 
+	    components << { name: strip_counts(row[res_col]), amount: get_counts(row[res_col])} 
 	  end
 	end
 	(12..16).each do |res_col|
 	  if row[res_col]
-	    @blob[t][type][new_name]["optionalResources"] << { name: strip_counts(row[res_col]), count: get_counts(row[res_col])} 
+	    components << { name: strip_counts(row[res_col]), amount: get_counts(row[res_col])} 
 	  end
 	end
+	@blob[t][parent_type][parent_recipe]["variants"] = Array.new if !@blob[t][parent_type][parent_recipe]["variants"]
+        if parent_recipe != new_name || (new_name.include?('Sigil') && components.first['name'] != "Ore")
+          @blob[t][parent_type][parent_recipe]["variants"] <<
+                {
+                  "name" => new_name,
+                  "attribute" => attribute || nil,
+                  "impact" => attribute_impact || nil,
+                  "success_chance" => row[2],
+                  "difficulty" => row[4],
+                  "components" => components
+                }
+        end
+	components = nil
+        # not 70?
       end
     end
     if !row[0] && !row[5] && !row[6] && !row[7] && !row[8] && !row[9] && !row[13] && !row[14]
       empty_row_count += 1
     end
   end
-  parent_recipe = nil
 end
 
-puts JSON.pretty_generate(JSON[@blob.to_json])
 
+@blob.each do |profession,types|
+  Dir.mkdir clean(profession) unless File.exists?(clean(profession))
+  types.each do |type,recipes|
+    recipes.each do |name, values|
+      #if !values["components"].empty?
+      File.open("#{clean(profession)}/#{clean(name)}.json", "w") do |f|
+        f.puts JSON.pretty_generate(values)
+      end
+    end
+  end
+end
+
+# output JSON to stdout for debugging
+puts JSON.pretty_generate(JSON[@blob.to_json])
